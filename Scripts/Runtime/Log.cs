fd6dc9b6ca8e3f3cf226391b43f07ebddf05643c
HomelessSkin <105777886+HomelessSkin@users.noreply.github.com>
2026-07-30 18:04:02 +0500

    122

--- Scripts/Runtime/Log.cs
+++ Scripts/Runtime/Log.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 using UnityEngine;
 using UnityEngine.Events;
@@ -54,6 +57,42 @@ namespace Core
 
             Debug.Log(text);
         }
+        public static void Object<T>(object agent, T target) where T : ILogTarget
+        {
+            Info(agent, $"-----------------------------");
+            if (target == null)
+            {
+                Error(agent, $"Target is Null!");
+                Info(agent, $"-----------------------------");
+
+                return;
+            }
+
+            var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+            var type = typeof(T);
+
+            var fields = type
+                .GetFields(bindingFlags)
+                .Where(f => f.GetCustomAttribute<LogInfo>() != null);
+
+            if (fields.Count() == 0)
+            {
+                Warning(agent, $"No marked Fields!");
+                Info(agent, $"-----------------------------");
+
+                return;
+            }
+
+            foreach (var field in fields)
+            {
+                var message = "";
+
+                FormatValue(field.GetValue(target), ref message);
+                Info(agent, $"{field.Name}: {message}");
+            }
+
+            Info(agent, $"-----------------------------");
+        }
 
         public static bool Read(out Message message)
         {
@@ -64,11 +103,67 @@ namespace Core
         }
         public static bool IsEmpty() => Q.Count == 0;
 
+        static void FormatValue(object value, ref string message)
+        {
+            if (value == null)
+            {
+                message += "null";
+
+                return;
+            }
+
+            if (value is string str)
+            {
+                message += $"\"{str}\"";
+
+                return;
+            }
+
+            if (value is ILogTarget target)
+            {
+                message += "{ ";
+
+                var type = target.GetType();
+                var fields = type
+                    .GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                    .Where(f => f.GetCustomAttribute<LogInfo>() != null);
+
+                foreach (var field in fields)
+                {
+                    message += field.Name + ": ";
+
+                    FormatValue(field.GetValue(target), ref message);
+                }
+
+                message += "}";
+
+                return;
+            }
+
+            if (value is IEnumerable<ILogTarget> list)
+            {
+                message += "[ ";
+
+                foreach (var item in list)
+                    FormatValue(item, ref message);
+
+                message += "]";
+
+                return;
+            }
+
+            message += value.ToString();
+        }
+
         public class Message
         {
             public string Text;
             public float Time;
             public float CallTime;
         }
+
+        public interface ILogTarget { }
+        [AttributeUsage(AttributeTargets.Field)]
+        public class LogInfo : Attribute { }
     }
 }
